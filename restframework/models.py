571c6ef155c8b637d19f5837949211edb437239e
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-07-13 16:27:18 +0300

    Serializar class and GET method

--- restframework/models.py
+++ restframework/models.py
@@ -1,3 +1,12 @@
+from re import M
+from statistics import mode
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    name= models.CharField(max_length=255)
+    price= models.FloatField()
+    stock= models.IntegerField()
+    description = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
--- restframework/views.py
+++ restframework/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from restframework.models import Book
+
+from restframework.serializers import BookSerializer
 
 # Create your views here.
+@api_view(['GET'])
+def getBooks(request):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
+
